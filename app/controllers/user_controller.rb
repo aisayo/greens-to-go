2254dc67343a1329fb7b9bd26c333bcd3408c0c1
Aysan Isayo <aisayo@icloud.com>
2020-03-26 09:57:39 -0700

    fixed logout feature

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
 class UserController < ApplicationController
 
     get '/users/signup' do 
-       erb :'/users/signup'
+      if !logged_in? 
+         erb :'/users/signup'
+      else  
+         redirect "/users/#{session[:user_id]}"
+      end 
     end 
 
     post '/users/signup' do
@@ -19,28 +23,20 @@ class UserController < ApplicationController
 
 
      get '/users/login' do 
-      #if the user is logged in
-      #redirect to their home page
-      # else, show them the login form 
       if !logged_in? 
          erb :'/users/login'
       else
          @user = User.find(session[:user_id])
          redirect "/users/#{@user.id}"
       end 
-         
      end 
 
 
      post '/users/login' do 
-      #want to find the user, if it exists 
       @user = User.find_by(username: params[:username])
-
       if @user && @user.authenticate(params[:password])
          session[:user_id] = @user.id 
          redirect "/users/#{@user.id}"
-         #authenticate password
-         #direct to homepage
       else  
          redirect "/users/login"
       end 
@@ -52,8 +48,8 @@ class UserController < ApplicationController
         erb :'/users/show'
      end 
 
-     get '/users/logout' do 
-      sessions.clear
+     get '/logout' do 
+      session.clear
       redirect '/users/login'
      end 
 
